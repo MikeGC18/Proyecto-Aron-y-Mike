3510c8faecb6b47f9859bffb6842fdb62d817a06
MikeGC18 <mrobertgc@gmail.com>
2025-11-06 23:14:47 +0100

    Eliminacion de proyecto no bueno

--- Proyecto bueno/FM1/Models/constructors.cs
+++
@@ -1,9 +0,0 @@
-namespace F1API.Models
-{
-    public class Constructor
-    {
-        public int ConstructorId { get; set; }
-        public string Name { get; set; }
-        public string Nationality { get; set; }
-    }
-}
